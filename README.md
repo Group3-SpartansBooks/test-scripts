c1bcdf32e2b1ce89700fe42ae28852869dc95c41
Vishnu Adda <addavishnu@gmail.com>
2020-05-12 17:33:48 -0700

    Add tests for login and logout functionality.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Scripts to do black-box and white-box testing
 
 # Black-box
 Note that you **must** have the [front-end](https://github.com/Group3-SpartansBooks/front-end) code running.
+
+### `npm test`
--- test/app.test.js
+++ test/app.test.js
@@ -14,3 +14,35 @@ describe('/GET homepage', () => {
             });
     });
 });
+
+describe('/login success', () => {
+    it('it should login', () => {
+        chai.request('http://localhost:3000')
+            .post('/login')
+            .type('form')
+            .send({
+                'email': 'vishnu.adda@sjsu.edu',
+                'password': 'Password'
+            })
+            .end((err, res) => {
+                chai.expect(res).to.exist;
+           });
+    })
+});
+
+describe('/logout success', () => {
+    it('it should logout', () => {
+        chai.request('http://localhost:3000')
+            .post('/login')
+            .type('form')
+            .send({
+                'email': 'vishnu.adda@sjsu.edu',
+                'password': 'Password'
+            })
+        chai.request('http://localhost:3000')
+            .post('/logout')
+            .end((err, res) => {
+                chai.expect(res).to.not.have.status(500);
+            });
+    });
+});
